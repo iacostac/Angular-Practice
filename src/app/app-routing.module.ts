edfae2317091c9548fa92a7d734c32cdfcfc36f4
iacostac <infoivan1@gmail.com>
2020-11-28 22:30:42 -0300

    Adding Home and brewerie components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { pathToFileURL } from 'url';
+import {HomeComponent} from './home/home.component';
+import {ListComponent} from './list/list.component';
+const routes: Routes = [
+  {
+    path: '',component : HomeComponent
+  },
 
-const routes: Routes = [];
+  {
+    path: 'list',component : ListComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
